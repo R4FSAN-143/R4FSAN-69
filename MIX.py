fed78acd3a2d495c4e38ba4e75ba73e1bac01ca4
R4FSAN-143 <124475382+R4FSAN-143@users.noreply.github.com>
2023-02-24 07:11:19 +0600

    Add files via upload

--- MIX.py
+++ MIX.py
@@ -89,7 +89,7 @@ bu = current.month
 ha = current.day
 today = date.today()
 os.system('xdg-open https://facebook.com/groups/32064142996CRAC/')
-logo =("""
+logo =("""\033[38;5;195m
   d8888b.  .d8b.  d88888b .d8888.  .d8b.  d8b   db 
 88  `8D d8' `8b 88'     88'  YP d8' `8b 888o  88 
 88oobY' 88ooo88 88ooo   `8bo.   88ooo88 88V8o 88 
